6d089db98e05d621f9fef3abf6b25bfea3495ffd
Shubham Sapra <shubham.sapra97@gmail.com>
2023-02-11 19:46:09 +0530

    chore: add dpk tests

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,37 @@
+const crypto = require("crypto");
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  describe("event partitionKey exists", () => {
+    it("does not convert candidate to string & length less than MAX_PARTITION_KEY_LENGTH", () => {
+      const event = {
+        partitionKey: "hi"
+      };
+      const trivialKey = deterministicPartitionKey(event);
+      expect(trivialKey).toBe("hi");
+    });
+
+    it("converts candidate to string & length less than MAX_PARTITION_KEY_LENGTH", () => {
+      const event = {
+        partitionKey: 12
+      };
+      const trivialKey = deterministicPartitionKey(event);
+      expect(trivialKey).toBe("12");
+    });
+  });
+
+  describe("event partitionKey does not exists", () => {
+    it("does not convert candidate to string & length less than MAX_PARTITION_KEY_LENGTH", () => {
+      const event = {
+        randomKey: "hi"
+      };
+      const trivialKey = deterministicPartitionKey(event);
+      expect(trivialKey).toBe(crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex"));
+    });
   });
 });
